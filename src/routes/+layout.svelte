5c9ff69a8ec7488b17e7300e4044596e39ab03d5
Blazskills <ilesanmiisaac@gmail.com>
2023-04-03 17:12:36 +0100

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import "modern-normalize/modern-normalize.css";
+    import '../styles/main.scss'
+    // import '../styles/app.css'
+
+</script>
+
+<slot/>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+Tailwind style <br/>
+<span class="text-[20px] font-bold underline text-[white]">Hello world!, tailwind css style</span>
+
+
+<p>
+    To activate the Tailwind css, uncomment tailwind style in <strong>src/routes/+layout.svelte</strong>
+</p>
+
+
+<br/>
+<br/>
+<br/>
+<br/>
+<br/>
+<input />
+
+<select name="" id="">
+	<option value="Option">Option</option>
+</select>
+<button>Button</button>
+
+<!-- cspell:ignore unsass -->
+<style lang="scss">
+	h1 {
+		color: rgb(255, 0, 251);
+		font-size: functions.toRem(36);
+		@include breakpoint.up('md') {
+			color: green;
+		}
+
+		@include breakpoint.up('lg') {
+			color: blue;
+		}
+		@include breakpoint.down('sm') {
+			color: indigo;
+		}
+		&:hover {
+			text-decoration: underline;
+			cursor: pointer;
+			color: rgb(174, 218, 0);
+		}
+	}
+</style>
